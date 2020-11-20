3f8dc944eb57d0a6374bcdef06480b895a647062
mgbardakov <mgbardakov@gmail.com>
2020-11-20 18:04:11 +0300

    2. Scriplet. Динамическая таблица. [#385297]

---
+++ src/main/java/ru/job4j/dream/model/Post.java
@@ -0,0 +1,59 @@
+package ru.job4j.dream.model;
+
+import java.util.Calendar;
+import java.util.Objects;
+
+public class Post {
+    private int id;
+    private String name;
+    private String description;
+    private final Calendar created;
+
+    public Post(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.created = Calendar.getInstance();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Calendar getCreated() {
+        return created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return id == post.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
+
