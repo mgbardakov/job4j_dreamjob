e1c9a673d1c44d4b8320b2dcb1b45aac0f7c914c
Maxim Bardakov <mgbardakov@gmail.com>
2021-02-09 14:57:13 +0300

    4.1. JSON формат. [#385284]

--- src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -1,21 +1,28 @@
 package ru.job4j.dream.servlet;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.stream.Collectors;
 
 public class GreetingServlet extends HttpServlet {
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/plain");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String data = req.getReader().lines().collect(Collectors.joining());
+        JsonObject json = new Gson().fromJson(data, JsonObject.class);
+        resp.setContentType("json");
         resp.setCharacterEncoding("UTF-8");
-        String name = req.getParameter("name");
         resp.setHeader("Access-Control-Allow-Origin", "*");
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.printf("{\"email\": \"%s\"}", name);
+        writer.print(json.toString());
         writer.flush();
     }
 }
